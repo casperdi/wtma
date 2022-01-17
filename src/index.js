2b5f7af0ddf26bf16d11621af3f3d3ca4fde3207
casperdi <cassu.dikert@gmail.com>
2022-01-17 20:09:09 +0200

    :)

--- src/index.js
+++ src/index.js
@@ -1,72 +1,73 @@
-const coursesEn = ["Hamburger, cream sauce and poiled potates",
-                "Goan style fish curry and whole grain rice",
-                "Vegan Chili sin carne and whole grain rice",
-                "Broccoli puree soup, side salad with two napas",
-                "Lunch baguette with BBQ-turkey filling",
-                "Cheese / Chicken / Vege / Halloum burger and french fries"];
-const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-                "Goalaista kalacurrya ja täysjyväriisiä",
-                "vegaani Chili sin carne ja täysjyväriisi",
-                "Parsakeittoa,lisäkesalaatti kahdella napaksella",
-                "Lunch baguette with BBQ-turkey filling",
-                "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+import LunchMenu from '../lunchmenu.json';
+console.log(LunchMenu);
+let itemEN;
+let itemFI;
+const coursesFI = [];
+const coursesEN = [];
 
-                
-                 document.getElementById('item').innerHTML = coursesFi; 
-                 let itemEN;
-                 let itemFI;
+for (let i = 1; i <= 9; i++) {
+  
+  coursesFI.push( LunchMenu.courses[i].title_fi);
+  console.log(coursesFI);
+  
+  coursesEN .push(LunchMenu.courses[i].title_en);
+  console.log(coursesEN);
+  
+};
 
-                 
-                const sortMenu = () => {
+document.getElementById('item').innerHTML = coursesFI;
+/* document.getElementById('item').innerHTML = coursesEN; */
+
+const langChange = () => {
+  if(document.getElementById('item').innerHTML == coursesEN || document.getElementById('item').innerHTML == itemEN){
+    document.getElementById('item').innerHTML = coursesFI;
+    
+  } else if (document.getElementById('item').innerHTML == coursesFI || document.getElementById('item').innerHTML == itemFI ){
+    document.getElementById('item').innerHTML = coursesEN;
+    
+  }
+};
+
+const sortMenuAsc = () => {
                   
-                  coursesEn.sort();
-                  coursesFi.sort();
-
-                  if(document.getElementById('item').innerHTML == coursesEn){
-                    document.getElementById('item').innerHTML = coursesEn;
-                    
-                  } else {
-                    document.getElementById('item').innerHTML = coursesFi;
-
-                  }
-
-
-                };
-                 
-
-                const langChange = () => {
-                  if(document.getElementById('item').innerHTML == coursesEn || document.getElementById('item').innerHTML == itemEN){
-                    document.getElementById('item').innerHTML = coursesFi;
-                    
-                  } else if (document.getElementById('item').innerHTML == coursesFi || document.getElementById('item').innerHTML == itemFI ){
-                    document.getElementById('item').innerHTML = coursesEn;
-
-                  }
-                };
-
-                
-
-                const random = () => {
-
-                  if( document.getElementById('item').innerHTML == itemEN || document.getElementById('item').innerHTML == coursesEn){
-                    itemEN = coursesEn[Math.floor(Math.random()*coursesEn.length)];
-                    document.getElementById('item').innerHTML = itemEN; 
-                  } else {
-                    itemFI = coursesFi[Math.floor(Math.random()*coursesFi.length)];
-                    document.getElementById('item').innerHTML = itemFI; 
-                  }
-                  
-                    
+  if(document.getElementById('item').innerHTML == coursesEN){
+    coursesEN.sort();
+    document.getElementById('item').innerHTML = coursesEN;
+    
+    
+  } else if (document.getElementById('item').innerHTML == coursesFI ) {
+    coursesFI.sort();
+    document.getElementById('item').innerHTML = coursesFI;
+  }
 
 
-                  
-                  
-                };
+};
+const sortMenuDesc = () => {
+              
+  if(document.getElementById('item').innerHTML == coursesEN){
+   coursesEN.sort();
+   coursesEN.reverse();
+   document.getElementById('item').innerHTML = coursesEN;
+     
+   } else if (document.getElementById('item').innerHTML == coursesFI){
+     coursesFI.sort();
+     coursesFI.reverse();
+     document.getElementById('item').innerHTML = coursesFI;
+   } 
+ };
 
-                document.getElementById('lang').addEventListener('click', langChange);
-                document.getElementById('sort').addEventListener('click', sortMenu);
-                document.getElementById('random').addEventListener('click', random);
-                
-                
-                  
-                
+ const random = () => {
+
+  if( document.getElementById('item').innerHTML == itemEN || document.getElementById('item').innerHTML == coursesEN){
+    itemEN = coursesEN[Math.floor(Math.random()*coursesEN.length)];
+    document.getElementById('item').innerHTML = itemEN; 
+  } else {
+    itemFI = coursesFI[Math.floor(Math.random()*coursesFI.length)];
+    document.getElementById('item').innerHTML = itemFI; 
+  }
+};
+
+document.getElementById('lang').addEventListener('click', langChange);
+document.getElementById('sorta').addEventListener('click', sortMenuAsc);
+document.getElementById('sortd').addEventListener('click', sortMenuDesc);
+document.getElementById('random').addEventListener('click', random);
